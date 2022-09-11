d21c2107e7b3261f8b9423c561529f4662333d68
Jenni Ferguson <98863330+jlferguson001@users.noreply.github.com>
2022-09-11 02:26:42 -0500

    finished controllers and routes

--- backend/routes/products.js
+++ backend/routes/products.js
@@ -1,76 +1,31 @@
 const express = require('express')
-const Product = require('../models/productModel')
+const { createProduct, getProduct, getProducts, getProductsMens, getProductsWomens, getProductsChildrens, deleteProduct, updateProduct } = require('../controllers/productController')
+
 
 const router = express.Router()
 
 //Get all products
-router.get('/', (req, res) => {
-  res.json({ message: 'GET all products' })
-})
+router.get('/', getProducts )
 
 //Get all Mens
-router.get('/Mens', (req, res) => {
-  res.json({ message: 'GET all Mens products' })
-})
+router.get('/Mens', getProductsMens)
 
 //Get all Womens
-router.get('/Womens', (req, res) => {
-  res.json({ message: 'GET all Womens products' })
-})
+router.get('/Womens', getProductsWomens )
 
-//Get all Childrents
-router.get('/Childrens', (req, res) => {
-  res.json({ message: 'GET all Childrens products' })
-})
+//Get all Childrens
+router.get('/Childrens', getProductsChildrens)
 
 //Get one product
-router.get('/:id', (req, res) => {
-  res.json({ message: 'Get a single product' })
-})
+router.get('/:id', getProduct)
 
 //post/add new product
-router.post('/', async (req, res) => {
-  const {
-    name,
-    image,
-    description,
-    brand,
-    category,
-    color,
-    price,
-    countInStock,
-    rating,
-    numReviews,
-  } = req.body
-
-  try {
-    const product = await Product.create({
-      name,
-      image,
-      description,
-      brand,
-      category,
-      color,
-      price,
-      countInStock,
-      rating,
-      numReviews,
-    })
-    res.status(200).json(product)
-  } catch (error) {
-    res.status(400).json({error: error.message})
-  }
-   
-})
+router.post('/', createProduct)
 
 //delete product
-router.delete('/:id', (req, res) => {
-  res.json({ message: 'Delete a single product' })
-})
+router.delete('/:id', deleteProduct )
 
 //edit/update product
-router.patch('/:id', (req, res) => {
-  res.json({ message: 'Update a single product' })
-})
+router.patch('/:id', updateProduct)
 
 module.exports = router
